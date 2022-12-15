1a36ae1312a128a4e4a6b3b0174f6bcaeff4ba8d
Hans Ellis <bullhorn@gmail.com>
2022-12-14 20:03:09 -0800

    add create view, index view, and corresponding controllers
    Co-authored-by: Quin Asselin <quin.asselin1@gmail.com>
    Co-authored-by: Jonathan Liera <jonliera1@gmail.com>

--- AnimalShelter/Controllers/AnimalController.cs
+++ AnimalShelter/Controllers/AnimalController.cs
@@ -19,5 +19,18 @@ namespace AnimalShelter.Controllers
       List<Animal> model = _db.Animals.ToList();
       return View(model);
     }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Animal animal)
+    {
+      _db.Animals.Add(animal);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
